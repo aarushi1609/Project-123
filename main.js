1eeab692dbfad740d9c47b2c8459ca465c6d6a67
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2022-05-18 18:55:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 function setup()
 {
+
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+
     canvas = createCanvas(300, 300);
-    canvas.center();
+    
+    PoseNet = ml5.poseNet(video, modelLoaded);
+    PoseNet.on("pose", gotResults);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+}
+
+function gotResults(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+
+function draw()
+{
+    
 }
